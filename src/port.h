a5b0d8ecc0d82cbf2af379024c21a1bb426b5392
huohuansong <huohuansong@outlook.com>
2023-09-19 06:49:22 +0800

    adapt hardware to support three channels

--- src/port.h
+++ src/port.h
@@ -10,9 +10,10 @@
 #define _PORT_H_
 
 /* Macro & Enum */
-#define PORT_NUM 2
+#define PORT_NUM 3
 #define PORT_PHYSICAL_PORT_1   0
 #define PORT_PHYSICAL_PORT_2   1
+#define PORT_PHYSICAL_PORT_3   2
 
 #define GET_UESR_PORT_FROM_PHYSICAL_PORT(phy_port) (phy_port + 1)
 #define GET_PHYSICAL_PORT_FROM_USER_PORT(user_port) (user_port - 1)
@@ -26,9 +27,11 @@
 
 #define SET_POWER_STATUS(phy_port, onoff) do { \
 		if (PORT_PHYSICAL_PORT_1 == phy_port) { \
-			P3_2 = (PORT_POWER_ON == onoff) ? 1 : 0; \
+			P3_2 = (PORT_POWER_OFF == onoff) ? 1 : 0; \
 		} else if (PORT_PHYSICAL_PORT_2 == phy_port)  { \
-			P3_3 = (PORT_POWER_ON == onoff) ? 1 : 0; \
+			P3_3 = (PORT_POWER_OFF == onoff) ? 1 : 0; \
+		} else if (PORT_PHYSICAL_PORT_3 == phy_port)  { \
+			P5_4 = (PORT_POWER_OFF == onoff) ? 1 : 0; \
 		} else { \
 			\
 		} \
